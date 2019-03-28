2cb6f6348327ce8630dad572510e5252f0f62426
saugustine <sangeetaaugustine@yahoo.com>
2019-03-28 17:38:04 +0000

    Create Exercise 1 - IfElse.py
    First ex done

---
+++ Exercise 1 - IfElse.py
@@ -0,0 +1,35 @@
+print('Welcome to the dungeon!')
+print('Do you go through the door 1 or door 2?')
+door = input('>')
+if door == "1":
+    print('There is a nice vampire asking you if you enjoy life.')
+    print('What do you do?')
+    print('1.Smile and nod')
+    print('2.Scream and run')
+    vampire = input(">")
+
+    if vampire == "1":
+        print('Congratulations, you found a new friend!')
+    elif vampire == "2":
+        print("Sorry the vampire is faster'.You become a dinner")
+    else:
+        print("You are not so good with numbers, are you?")
+
+
+    if door =='2':
+        print("There is a nice vampire asking you if you enjoy life.")
+        print('What do you do?')
+        print('1.Smile and nod')
+        print('2.Scream and run')
+        vampire = input(">")
+
+    if vampire == "1":
+        print('Congratulations, you found a new friend!')
+    elif vampire == "2":
+        print("Sorry the vampire is faster'.You become a dinner")
+    else:
+        print("You are not so good with numbers, are you?")
+
+else:
+        print("You are not so good with numbers, are you?")
+
